6385fe49f73ed10d52cf98141589f609ec62f6e1
Jordan Lawler <jordan.lawler@colorado.edu>
2015-12-09 14:00:12 -0700

    simulator finished

---
+++ test/simulation_environment/test_simulation.rb
@@ -0,0 +1,16 @@
+require_relative '../test_helper'
+require_relative 'client_environment_simulation'
+
+class SimulationTest < ControllerTestSetup
+
+  def test_simulation
+    ces = ClientEnvironmentSimulator.new
+    ces.start_simulation
+
+    binding.pry
+  end
+
+
+
+
+end
