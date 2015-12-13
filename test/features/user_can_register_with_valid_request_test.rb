d670a11bf434bd0f12f6b704f5bb0e804c3aaae6
Joe Perry <joseph.w.perry@gmail.com>
2015-12-13 15:14:49 -0700

    minor changes

---
+++ test/features/user_can_register_with_valid_request_test.rb
@@ -0,0 +1,14 @@
+require_relative '../test_helper'
+require_relative '../simulation_environment/test_data.rb'
+
+class UserCanRegisterWithValidRequestTest < FeatureTest
+
+  # def setup
+  #   td = TestData.new(2)
+  #   td.generate
+  # end
+
+
+
+
+end
