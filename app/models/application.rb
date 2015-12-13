da46b07379ae7bfbef5070d5cede9af2ef6bb613
Jordan Lawler <jordan.lawler@colorado.edu>
2015-12-13 13:24:55 -0700

    updated app view

--- app/models/application.rb
+++ app/models/application.rb
@@ -13,15 +13,15 @@ class Application < ActiveRecord::Base
   end
 
   def user_agents
-    payloads.group(:user_agent).count.sort_by{|a,b| (b * -1)}.to_h
+    payloads.group(:user_agent).count.sort_by{|a,b| (b * -1)}.to_h.map{|a, b| {:user_agent => a[:user_agent], :os => a[:os]}}
   end
 
   def screen_resolutions
-    payloads.group(:screen_resolution).count.sort_by{|a,b| (b * -1)}.to_h
+    payloads.group(:screen_resolution).count.sort_by{|a,b| (b * -1)}.to_h.map{|a, b| {:screen_resolution => [a[:width],a[:height]]}}
   end
 
   def average_response_times
-    payloads.group(:url).average(:responded_in).sort_by{|a,b| (b * -1)}.to_h
+    payloads.group(:url).average(:responded_in).sort_by{|a,b| (b * -1)}.to_h.map{|a, b| [a[:url], b]}
   end
 
   def sorted_events
--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -15,12 +15,12 @@ class ApplicationDetailsTest < ControllerTestSetup
 
   def test_app_returns_user_agent_breakdown_across_all_requests
     app = Application.find_by(identifier: 'google')
-    assert_equal "Chrome 24.0.1309", app.user_agents.keys[0][:user_agent]
+    assert_equal "Chrome 24.0.1309", app.user_agents[0][:user_agent]
   end
 
   def test_os_breakdown_accross_all_requests
     app = Application.find_by(identifier: 'google')
-    assert_equal "Mac OS X 10.8.2", app.user_agents.keys[0][:os]
+    assert_equal "Mac OS X 10.8.2", app.user_agents[0][:os]
   end
 
   def test_screen_reso_breakdown_accross_all_requests
@@ -31,9 +31,9 @@ class ApplicationDetailsTest < ControllerTestSetup
 
   def test_average_response_time
     app = Application.find_by(identifier: 'google')
-    average_response_time = app.average_response_times.to_a
-    assert_equal "images", average_response_time[0][0][:url]
-    assert_equal 5.0, average_response_time[0][1].to_f
+    average_response_time = app.average_response_times
+    assert_equal 'images', average_response_time[0][0]
+    assert_equal 5.0, average_response_time[1][1]
   end
 
   def test_event_returns_sorted_event_data
