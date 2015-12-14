d2e00fdc4b928cb086ad98992e795fc95962f9c0
Joe Perry <joseph.w.perry@gmail.com>
2015-12-14 00:39:21 -0700

    identifier event data feature testing

---
+++ test/features/user_can_access_identifier_info_from_home_test.rb
@@ -0,0 +1,46 @@
+require_relative '../test_helper'
+
+class UserCanAccessIdentifierInfoFromHomeTest < FeatureTest
+
+  def setup
+    post '/sources', "identifier=ultratest&rootUrl=http://ultratest.com"
+
+    payload_data = {"url": "http://ultratest.com/tester",
+                    "requestedAt": "2015-11-23 11:23:15 -0700",
+                    "respondedIn": 100,
+                    "referredBy": "http://google.com",
+                    "requestType": "POST",
+                    "parameters": [],
+                    "eventName": "socialLogin",
+                    "userAgent": "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                    "resolutionWidth": 1920,
+                    "resolutionHeight": 1080,
+                    "ip": "126.127.128"}.to_json
+
+                    post "/sources/ultratest/data", {payload: payload_data}
+  end
+
+  def test_user_can_access_identifier_info_from_home
+      visit "/"
+      assert_equal "/", current_path
+      within("#main_page_heading") do
+        assert page.has_content?("Traffic Spy")
+      end
+
+      fill_in('identifier', :with => 'ultratest')
+      click_button('Submit')
+
+      assert_equal "/sources/ultratest", current_path
+
+      within("#app_details_main") do
+        assert page.has_content?("ultratest")
+        assert page.has_content?("tester : 1")
+        assert page.has_content?("Mac OS X 10.8.2")
+        assert page.has_content?("Chrome 24.0.1309")
+        assert page.has_content?("Longest Average Response Time: 100.0")
+        assert page.has_content?("Shortest Average Response Time: 100.0")
+        assert page.has_content?("Screen Resolutions: 1920 by 1080")
+      end
+  end
+
+end
--- test/features/user_can_view_data_for_path_test.rb
+++ test/features/user_can_view_data_for_path_test.rb
@@ -11,7 +11,7 @@ class UserCanViewDataForPathTest < FeatureTest
                     "referredBy": "http://google.com",
                     "requestType": "POST",
                     "parameters": [],
-                    "eventName": "#socialLogin",
+                    "eventName": "socialLogin",
                     "userAgent": "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
                     "resolutionWidth": 1920,
                     "resolutionHeight": 1080,
